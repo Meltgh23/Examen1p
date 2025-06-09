0101df7349a418a3d919a327eff77c8f076b2517
MelvinMeltgh23 <melvingutierrez3302@gmail.com>
2025-06-09 08:51:26 -0600

    este es el Primer commit

---
+++ firebase.jsx
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-eB9eP8wPPK3wBlYAxEjWM70RMxEHYIg",
+  authDomain: "examenp1-dac57.firebaseapp.com",
+  projectId: "examenp1-dac57",
+  storageBucket: "examenp1-dac57.firebasestorage.app",
+  messagingSenderId: "236170850277",
+  appId: "1:236170850277:web:6c26fd7b7f3ef7fd6dc90c"
+};
+
+
+const app = initializeApp(firebaseConfig);
